6cf527676cbba1f6e474a58305b11d6e5abc7315
Code1357 <61643130+Code1357@users.noreply.github.com>
2020-07-21 07:03:45 +0900

    care_record_00042

--- care_record/controllers/careRecordController.js
+++ care_record/controllers/careRecordController.js
@@ -181,16 +181,10 @@ module.exports = {
     });
   },
   newRecordConfirmation: (req, res, next) => {
-    const userId = req.params.user_id;
     if (req.body.action2) {
       delete req.body.action2;
-      const newRecUserConfirmation = JSON.parse(JSON.stringify(req.body));
-      res.redirect(`/careRecords/recordPage/${userId}`, { newRecUserConfirmation: newRecUserConfirmation }); ///// ここの工夫！後一息！！
+    } else {
       delete req.body.action;
-      // console.log(req.body);
-      // const newUserConfirmation = JSON.parse(JSON.stringify(req.body));
-      // res.locals.newUserConfirmation = newUserConfirmation;
-      // console.log(newConfirmation);
       next();
     };
   },
@@ -203,10 +197,98 @@ module.exports = {
     con.query(sql, [recordDate, record, userId, staffId], (error, results) => {
       if (error) {
         req.flash('error', '登録できませんでした,もう一度登録しなおしてください');
+        console.log(results);
         res.redirect(`/careRecords/recordPage/${userId}`);
       } else {
+        req.flash('success', '記録の登録完了');
         res.redirect(`/careRecords/users/${userId}`);
       }
     });
+  },
+  searchPage: (req, res) => {
+    con.query('select * from user_lists order by user_name', (err, result, fields) => {
+      if (err) throw err;
+      res.locals.userList = result;
+      {
+        const userId = req.params.user_id;
+        res.locals.userId = userId;
+        con.query('select user_name from user_lists where user_id = ?', userId, (err, result, fields) => {
+          if (err) throw err;
+          // console.log(staffName);
+          res.locals.userName = result[0].user_name;
+          // console.log(res.locals.staffName);
+
+          con.query('select u.user_name, a.adl from user_lists as u join adls as a on u.adls_adl_id = a.adl_id where user_id = ?', userId, (err, result, fields) => {
+            if (err) throw err;
+            const adl = result[0].adl;
+            res.locals.adl = adl;
+
+            con.query('select u.user_name, g.gender from user_lists as u join genders as g on u.genders_gender_id = g.gender_id where user_id = ?', userId, (err, result, fields) => {
+              if (err) throw err;
+              // console.log(result);
+              const gender = result[0].gender;
+              res.locals.gender = gender;
+              res.render('careRecords/recordSearch');
+            });
+          });
+        });
+      }
+    });
+  },
+  search: (req, res) => {
+    const userId = req.params.user_id;
+    const searchDate = req.query.search_date;
+    console.log(req.query.search_date);
+    con.query('select user_lists_user_id, recording_date, care_record, care_records_staff_id from care_records where cast(recording_date as date ) = ? and user_lists_user_id = ?', [searchDate, userId], (err, result, fields) => {
+      if (err) throw err;
+      res.locals.pastRecord = result;
+      console.log(result);
+      res.render('careRecords/pathRecord');
+      result.forEach(result => {
+        const a = result.recording_date;
+        const b = result.care_records_staff_id.toString();
+        const c = [a, b];
+        console.log(c);
+
+        // or
+        console.log([result.care_records_staff_id.toString(), result.recording_date]);　//　でやってみる
+
+      });
+      
+    
+
+     /*  const recordingDate = `${resultEntranceData.getUTCFullYear()}年${resultEntranceData.getUTCMonth() + 1}月${resultEntranceData.getUTCDate()}日`;
+            res.locals.entranceData = entranceData; */
+
+
+// joinする
+      /* con.query('select u.user_name, g.gender from user_lists as u join genders as g on u.genders_gender_id = g.gender_id where user_id = ?', userId, (err, result, fields) => {
+        if (err) throw err; */
+
+    });
   }
 };
+
+
+/*
+[
+  TextRow {
+    user_lists_user_id: 1,                     // 利用者氏名は、user_id活用にて別sqlでレンダリング
+    recording_date: 2020-07-20T21:21:00.000Z,  // 
+    care_record: 'おはようございます。',          // 
+    care_records_staff_id: 777777              // 連想配列から抽出し、joinで記録者名を取得
+  },
+  TextRow {
+    user_lists_user_id: 1,
+    recording_date: 2020-07-21T02:26:00.000Z,
+    care_record: 'こんにちわ。',
+    care_records_staff_id: 777777
+  },
+  TextRow {
+    user_lists_user_id: 1,
+    recording_date: 2020-07-21T10:21:00.000Z,
+    care_record: 'こんばんわ。',
+    care_records_staff_id: 777777
+  }
+]
+*/
--- care_record/routes/careRecordRoutes.js
+++ care_record/routes/careRecordRoutes.js
@@ -13,5 +13,7 @@ router.get('/updatePage/:user_id', careRecordController.updatePage);
 router.post('/update/:user_id', careRecordController.update);
 router.get('/recordPage/:user_id', careRecordController.recordPage);
 router.post('/recordCreate/:user_id', careRecordController.newRecordConfirmation, careRecordController.recordCreate);
+router.get('/searchPage/:user_id', careRecordController.searchPage);
+router.get('/recordSearch/:user_id', careRecordController.search);
 
 module.exports = router; // routes/index.jsにloadさせる
